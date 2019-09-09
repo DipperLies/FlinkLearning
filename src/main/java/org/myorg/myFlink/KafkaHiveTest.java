4be810cd5ff21889ef4277e464cbd43aecfacc07
wangzichao <wangzichao01@tcl.com>
2019-09-09 15:37:08 +0800

    flink读取kafka数据写入HDFS

--- src/main/java/org/myorg/myFlink/KafkaHiveTest.java
+++ src/main/java/org/myorg/myFlink/KafkaHiveTest.java
@@ -1,26 +1,30 @@
 package org.myorg.myFlink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-
-
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
+import org.apache.flink.streaming.connectors.fs.StringWriter;
+import org.apache.flink.streaming.connectors.fs.bucketing.BucketingSink;
+import org.apache.flink.streaming.connectors.fs.bucketing.DateTimeBucketer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
 
+
+import java.time.ZoneId;
 import java.util.Properties;
 
 /**
  * @author Michael
  * @date 2019-09-05 16:11
  */
-public class KafkaTest {
+public class KafkaHiveTest {
     public static void main(String[] args ) throws Exception{
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(1000);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         Properties properties=new Properties();
         properties.setProperty("bootstrap.servers","10.108.240.137:9092,10.108.240.147:9092,10.108.240.157:9092");
@@ -30,9 +34,18 @@ public class KafkaTest {
         FlinkKafkaConsumer08<String> myConsumer =new FlinkKafkaConsumer08<String>("P2OEEMDB.EQP_STATE_MPA",new SimpleStringSchema(),
                 properties);
         DataStream<String> stream = env.addSource(myConsumer);
-        DataStream<Tuple2<String,Integer>> counts=stream.flatMap(new LineSplitter()).keyBy(0).sum(1);
+//        DataStream<Tuple2<String,Integer>> counts=stream.flatMap(new LineSplitter()).keyBy(0).sum(1);
 
-        counts.print();
+        stream.print();
+
+        System.out.println("*********** hdfs ***********************");
+        BucketingSink<String> sink = new BucketingSink<String>("hdfs://10.108.7.181:8020/tmp/path");
+        sink.setBucketer(new DateTimeBucketer<>("yyyy-MM-dd--HHmm", ZoneId.of("Asia/Shanghai")));
+        sink.setWriter(new StringWriter<>());
+        sink.setBatchSize(1024 * 1024 * 400 ); // this is 400 MB,
+        sink.setBatchRolloverInterval(10 * 60 * 1000); // this is 20 mins
+
+        stream.addSink(sink);
         env.execute("Word Count Kafka Test");
 
     }
@@ -51,4 +64,5 @@ public class KafkaTest {
             }
         }
     }
+
 }
